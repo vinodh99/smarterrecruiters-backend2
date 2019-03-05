3245753a3e4c5813b3d743dba769d4f4e1d7c295
nithinganji <nithin3287@gmail.com>
2019-03-05 16:18:34 -0500

    updated

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-// var db = mongoose.connect('mongodb://localhost/swag-shop');
-var db = mongoose.connect('mongodb://vinodh:bhuvin99@ds145669.mlab.com:45669/employeedata');
-
-var Product = require('./model/product');
-var WishList = require('./model/wishlist');
+var request = require('request');
+var isJSON = require('is-json');
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*'); // * => allow all origins
@@ -19,120 +15,49 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.listen(process.env.PORT || 3010,function(){
-    console.log("swag shop API running on port 3010");
+    console.log("Server running on port 3010");
 });
 
-app.post('/product',function(req,res){
-    var product = new Product();
-    product.firstName = req.body.firstName;
-    product.lastName = req.body.lastName,
-    product.dob = req.body.dob,
-    product.homePhone= req.body.homePhone,
-    product.workPhone = req.body.workPhone
-    product.title = req.body.title; 
-    product.price = req.body.price;
-    product.imgUrl = req.body.imgUrl;
-    console.log(req.body.imgUrl);
-
-    // product.populate(req.body.imgUrl);
-
-    //product.imgUrl = req.body.imgUrl;
-    product.save(function(err, savedProduct){
-        if (err){
-            res.status(500).send({error:'could not save product'});
-        }else{
-            res.send(savedProduct);
-        }
-    });
+app.get('/results',function(req,res){
     
-});
+    var pageNum = req.query.pageNum;
+    var limit = req.query.limit;
 
-app.put('/product', function(req,res){
-// Product.findOne({_id: req.body.productId}, function(err, product){
+    var options = {
+        uri: 'http://mock-api.smartermeasure.com/v4/results',
+        qs:{'page':pageNum,'page_size':limit},
+        method: 'GET'
+    }
 
-//     console.log(req.body.productId);    
-
-    Product.findOneAndUpdate({_id: req.body.productId}, {$set: {imgUrl: req.body.imgUrl}},(err,data)=>{
-        console.log(data);
-        console.log(err);
-    });
-
-
-//         if(err){
-//             res.send({error:'could not update the item'});
-//         }else{
-//             Product.findAndModify({_id: req.body.productId}, {$set: {imgUrl: req.body.imgUrl}});
-//         }
-    // })
-        //res.body = "Done";
-    return res.send("Done")
- });
-
-app.get('/product',function(req,res){
-    Product.find({firstName: req.query.firstName},function(err,products){
-        if(err){
-            res.send({error:'could not fetch products'});
+    request(options,function(error,response,body){
+        if(body === undefined || !isJSON(body)){
+            res.send({status:404,data:'mock api might not be available'});
+        }else if(response){
+            res.send({status:response.statusCode,data:JSON.parse(response.body)});
         }else{
-            res.send(products);
+            res.send({status:error.statusCode,data:'error while fetching response'});
         }
     });
-
 });
 
-app.get('/products',function(req,res){
-    Product.find({},function(err,products){
-        if(err){
-            res.send({error:'could not fetch products'});
+app.get('/users/:id',function(req,res){
+
+    var userId = req.params.id;
+
+    var options = {
+        uri: 'http://mock-api.smartermeasure.com/v4/users/'+userId,
+        method: 'GET'
+    }
+
+    request(options,function(error,response,body){
+        if(body === undefined || !isJSON(body)){
+            res.send({status:404,data:'mock api might not be available'});
+        }else if(response){
+            res.send({status:response.statusCode,data:JSON.parse(response.body)});
         }else{
-            res.send(products);
+            res.send({status:error.statusCode,data:'error while fetching response'});
         }
     });
-
-});
-
-app.post('/wishList',function(req,res){
-    var wishList = new WishList();
-    wishList.title = req.body.title;
-    wishList.save(function(err,newWishlist){
-        if(err){
-            res.send({error:'could not fetch products'});
-        }else{
-            res.send(newWishlist);
-        }
-    })
-});
-
-
-app.get('/wishlist', function(req,res){
-    WishList.find({}).populate({path: 'products',model:'Product'}).exec(function(err,wishLists)
-    {
-        if(err){
-        res.status(500).send({error: 'could not fetch wishlists'});
-        }else{
-            res.send(wishLists);
-        }
-    })
-});
-//we are defining the path and model that has to be populated. Path comes from the wishlist.js file 
-// which contains 'products' while model is retrive from the product.js where the fields or data of 
-// the object are stored.Then we execute using the exec keyword and return the wishlists along with the data.
-
-
-app.put('/wishlist/add',function(req,res){
-    Product.findOne({_id: req.body.productId}, function(err, product){
-        if(err){
-            res.send({error:'could not add item to the wishlist'});
-        }else{
-            WishList.update({_id: req.body.wishListId}, {$addToSet: {products: product._id}})
-//            ,function(err,wishList){
-//               if(err){
-//                   res.send({error:'wishlist could not be updated'});
-//               }else{
-//                   res.send("added to wishlist successfully");
-//               }
-            }//updating the user specified wishlistId with the found product
-        })
-    })
-
+})
 
 
